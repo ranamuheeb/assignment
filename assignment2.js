f750221593b4b9ca9fe799fdf055a5eb3c1bbf04
ranamuheeb <139947185+ranamuheeb@users.noreply.github.com>
2023-07-23 02:32:49 -0700

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,6 @@
+"use strict";
+//Store a person name in a variable and print a message to that person.
+//Personal message.
+let personname = "Rana Muheeb";
+let message = `${personname}"Hello, would you like learn some python today?"`;
+console.log(message);
